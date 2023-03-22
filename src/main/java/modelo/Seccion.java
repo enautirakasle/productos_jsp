660548f8c1585aef1b7880e68839bb68079cd7bc
Eñaut Agirre Arri <enaut.irakasle@gmail.com>
2023-03-22 19:22:27 +0100

    first commit

---
+++ src/main/java/modelo/Seccion.java
@@ -0,0 +1,21 @@
+package modelo;
+
+public class Seccion {
+	private int id;
+	private int nombre;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getNombre() {
+		return nombre;
+	}
+	public void setNombre(int nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+}
